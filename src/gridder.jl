8e1c360c568a85be297e17aa1bf582b09c9b2957
Torrance Hodgson <torrance@pravic.xyz>
2021-11-03 16:43:58 +0800

    Use 'standard order' for DFT to avoid one fftshift; take defn of ndash from IDG; grid onto Float64 grid

--- src/gridder.jl
+++ src/gridder.jl
@@ -1,4 +1,4 @@
-using FFTW: fft, fftshift, ifftshift
+using FFTW: fft, fftshift, ifftshift, fftfreq
 
 function parallelgrid!(mastergrid, subgrids, c)
     ch = Channel{Tuple{Subgrid, Task}}(1000)
@@ -21,32 +21,45 @@ end
 
 
 function gridder(subgrid::Subgrid, gridspec, subgridspec, c)
-    grid = zeros(ComplexF32, subgridspec.Nx, subgridspec.Ny)
+    grid = zeros(ComplexF64, subgridspec.Nx, subgridspec.Ny)
     dft!(grid, subgrid, gridspec, subgridspec, c)
-    return ifftshift(fft(ifftshift(grid))) ./ (subgridspec.Nx * subgridspec.Ny)
-end   
+    return fftshift(fft(grid)) ./ (subgridspec.Nx * subgridspec.Ny)
+end
 
-function dft!(grid::Matrix{ComplexF32}, subgrid::Subgrid, gridspec::GridSpec, subgridspec::GridSpec, c)
-    N = subgridspec.Nx
-    lms = px2sky(N, subgridspec)
+function dft!(grid::Matrix{ComplexF64}, subgrid::Subgrid, gridspec::GridSpec, subgridspec::GridSpec, c)
+    lms = fftfreq(subgridspec.Nx, 1 / subgridspec.scaleuv)
 
     u0px = subgrid.u0px + (subgridspec.Nx ÷ 2) # Central 'zeroth' pixel
     v0px = subgrid.v0px + (subgridspec.Ny ÷ 2)
     u0lambda, v0lambda = px2lambda(u0px, v0px, gridspec)
 
     for (mpx, m) in enumerate(lms), (lpx, l) in enumerate(lms)
-        n2 = 1 - l^2 - m^2
-        if n2 >= 0
-            ndash = sqrt(n2) - 1
-            for uvdatum in subgrid.children
-                phase = 2π * 1im * (
-                    (uvdatum.u - u0lambda) * l +
-                    (uvdatum.v - v0lambda) * m +
-                    (uvdatum.w - subgrid.w0lambda) * ndash
-                )
-                grid[lpx, mpx] += 0.5 * (uvdatum.xx + uvdatum.yy) * exp(phase)
-            end
-            grid[lpx, mpx] *= c(l, m)
+        for uvdatum in subgrid.children
+            phase = 2π * 1im * (
+                (uvdatum.u - u0lambda) * l +
+                (uvdatum.v - v0lambda) * m +
+                (uvdatum.w - subgrid.w0lambda) * ndash(l, m)
+            )
+            grid[lpx, mpx] += 0.5 * (uvdatum.xx + uvdatum.yy) * exp(phase)
         end
+        grid[lpx, mpx] *= c(l, m)
+    end
+end
+
+"""
+    Calculate n' = 1 - n
+                 = 1 - √(1 - l^2 - m^2)
+                 = (l^2 + m^2) / (1 + √(1 - l^2 - m^2))
+
+    The final line is (apparently) more accurate for small values of n.
+
+    Note for values of n > 1, which are unphysical, ndash is set to 1.
+"""
+@inline function ndash(l, m)
+    r2 = l^2 + m^2
+    if r2 > 1
+        return 1
+    else
+        return r2 / (1 + sqrt(1 - r2))
     end
 end
